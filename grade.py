e7ddc583775c72a13e08769b087a90687062ae36
Quentin Lurkin <qlurkin@gmail.com>
2020-06-24 00:48:13 +0200

    Add fill_excel and save_grade

--- grade.py
+++ grade.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from openpyxl import load_workbook
 
 def load_grade(path):
 	dt = pd.read_csv(path, sep=';', dtype={'matricule': object})
@@ -7,3 +8,34 @@ def load_grade(path):
 
 def capwords(S):
 	return ' '.join([w.capitalize() for w in S.split(' ')])
+
+def save_grade(df, path):
+	 df.to_csv(path, sep=';', index_label='matricule')
+
+def fill_excel(df, excel, firstRow=4, matriculeColumn='B', gradeColumn='D', statusColumn='E', dfColumn='grade'):
+	wb = load_workbook(filename=excel)
+	sheets = list(wb.sheetnames)
+	sheets.remove('NE_PAS_EFFACER')
+	ws = wb[sheets[0]]
+	lastRow = int(''.join(list(filter(lambda l: l in '0123456789', ws.calculate_dimension().split(':')[1]))))
+	index = {}
+	for i in range(firstRow, lastRow+1):
+		index[ws[matriculeColumn+str(i)].value] = gradeColumn+str(i)
+	
+	def fun(row):
+		matricule = int(row.name)
+		if matricule in index:
+			if ws[index[matricule]].value is None:
+				ws[index[matricule]] = row[dfColumn]
+			else:
+				print(f'{matricule} already get grade')
+			del index[matricule]
+		else:
+			print(f'{matricule} not in file')
+
+	df.apply(fun, axis=1)
+
+	for matricule in index:
+		print(f'{matricule} don\'t have grade')
+
+	wb.save(filename=excel)
